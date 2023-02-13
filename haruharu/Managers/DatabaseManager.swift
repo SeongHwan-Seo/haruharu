711d155fbf554a5316dd10e4c0c281f2064c4a45
SeongHwan-Seo <seosh5704@gmail.com>
2023-02-13 20:57:57 +0900

    Feat: 습관등록기능 추가

--- haruharu/Managers/DatabaseManager.swift
+++ haruharu/Managers/DatabaseManager.swift
@@ -6,20 +6,29 @@
 //
 
 import RealmSwift
+import Foundation
 
 class DatabaseManager {
     static let shared = DatabaseManager()
     
     private let realm = try! Realm()
     
+    
     // MARK: - Lifecycle
     
     private init() {}
     
-    func addUser(_ user: Object) {
-        do {
-            realm.add(user)
+    func createHabit(_ habitName: String, _ goalDay: Int) {
+        
+        let habit = Habit()
+        habit.habitName = habitName
+        habit.goalDay = goalDay
+        
+        try! realm.write {
+            realm.add(habit)
         }
+        
+        
     }
     
 }
