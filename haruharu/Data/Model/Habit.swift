e0ff9ea6c39e43d424083fe74d4c7e787ef067fb
SeongHwan-Seo <seosh5704@gmail.com>
2023-02-28 16:55:17 +0900

    Fix: 습관 데이터 프로퍼티 추가

--- haruharu/Data/Model/Habit.swift
+++ haruharu/Data/Model/Habit.swift
@@ -13,14 +13,17 @@ class Habit: Object {
     @Persisted var habitName: String
     @Persisted var goalDay: Int
     @Persisted var createdDate: String
+    @Persisted var isAlarm: Bool
+    @Persisted var alarmTime: String
     @Persisted var startDays: List<HabitDetail>
     
-    convenience init(id: Int, habitName: String, goalDay: Int, createdDate: String, startDays: List<HabitDetail>) {
+    convenience init(id: Int, habitName: String, goalDay: Int, createdDate: String, isAlarm: Bool,startDays: List<HabitDetail>) {
         self.init()
         
         self.habitName = habitName
         self.goalDay = goalDay
         self.createdDate = createdDate
+        self.isAlarm = isAlarm
         self.startDays = startDays
     }
     
