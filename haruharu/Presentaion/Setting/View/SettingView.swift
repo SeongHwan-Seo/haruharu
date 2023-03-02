3c764ca27c1810aae9dd5c443f06d6ff87a91a6a
SeongHwan-Seo <seosh5704@gmail.com>
2023-03-02 18:00:44 +0900

    Design: 런치 스크린 UI 작성

---
+++ haruharu/Presentaion/Setting/View/SettingView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingView.swift
+//  haruharu
+//
+//  Created by SHSEO on 2023/03/02.
+//
+
+import Foundation
---
+++ haruharu/Presentaion/Setting/ViewController/SettingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SettingViewController.swift
+//  haruharu
+//
+//  Created by SHSEO on 2023/03/02.
+//
+
+import Foundation
